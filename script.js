d0a979dc88893e1c06c586cb6e8215b033c1e96b
João Henrique <108812931+JoaoHenriquelm@users.noreply.github.com>
2024-04-18 09:27:18 -0300

    bug de aumento de intervalo
    Bug de mais tempo no intervalo, talvez resolvido

--- script.js
+++ script.js
@@ -1,132 +1,97 @@
-var pomodoroTimer = document.getElementById("pomodoro-timer")
-var pomodoroStart = document.getElementById("pomodoro-start")
-var pomodoroPause = document.getElementById("pomodoro-pause")
-var pomodoroTimerTitle = document.getElementById("pomodoroTimer")
-var shortBreakTimerTitle = document.getElementById("shortBreakTimer")
-pomodoroTimerTitle.classList.add("ativo")
-pomodoroTimer.innerText = "25:00"
+const pomodoroTimer = document.getElementById("pomodoro-timer");
+const pomodoroStart = document.getElementById("pomodoro-start");
+const pomodoroPause = document.getElementById("pomodoro-pause");
+const pomodoroTimerTitle = document.getElementById("pomodoroTimer");
+const shortBreakTimerTitle = document.getElementById("shortBreakTimer");
+pomodoroTimerTitle.classList.add("ativo");
+pomodoroTimer.innerText = "25:00";
+let pausa;
 
-var minutos = 0
-var segundos = 0
-var contador = 1
+let minutos = 0;
+let segundos = 0;
+let contador = 1;
+
+
 
 function shortBreakPage() {
-  pomodoroStart.style.display = "block"
-  pomodoroPause.style.display = "none"
-  pomodoroTimerTitle.classList.remove("ativo")
-  shortBreakTimerTitle.classList.add("ativo")
-  contador = 2
-  pomodoroTimer.innerText = "05:00"
-  minutos = 5
-  segundos = 0
+  clearInterval(pausa)
+  pomodoroStart.style.display = "block";
+  pomodoroPause.style.display = "none";
+  pomodoroTimerTitle.classList.remove("ativo");
+  shortBreakTimerTitle.classList.add("ativo");
+  contador = 2;
+  pomodoroTimer.innerText = "05:00";
+  minutos = 5;
+  segundos = 0;
 }
 
 function pomodoroPage() {
-  pomodoroStart.style.display = "block"
-  pomodoroPause.style.display = "none"
-  shortBreakTimerTitle.classList.remove("ativo")
-  pomodoroTimerTitle.classList.add("ativo")
-  contador = 2
-  pomodoroTimer.innerText = "25:00"
-  minutos = 25
-  segundos = 0
+  clearInterval(pausa)
+  pomodoroStart.style.display = "block";
+  pomodoroPause.style.display = "none";
+  shortBreakTimerTitle.classList.remove("ativo");
+  pomodoroTimerTitle.classList.add("ativo");
+  contador = 2;
+  pomodoroTimer.innerText = "25:00";
+  minutos = 25;
+  segundos = 0;
 }
 
 function startPomodoro() {
-  let pausa = setInterval(() => {
     if (pomodoroTimerTitle.classList.contains("ativo")) {
-      pomodoro()
-      function pomodoro() {
-        if (minutos == 0 && segundos == 0 && contador == 1) {
-          minutos = 25
-          segundos = 0
-          contador = 2
-        } else if (segundos <= 0 && minutos <= 0 && contador == 2) {
-          contador = 1
-          clearInterval(pausa)
-          pomodoroPause.style.display = "none"
-          pomodoroStart.style.display = "block"
-          pomodoroTimerTitle.classList.remove("ativo")
-          shortBreakTimerTitle.classList.add("ativo")
-        } else if (pomodoroStart.style.display == "block") {
-          clearInterval(pausa)
-        } else if (
-          segundos > 0 &&
-          minutos > -1 &&
-          pomodoroStart.style.display == "none"
-        ) {
-          --segundos
-        } else if (segundos == 0) {
-          --minutos
-          segundos = 59
-        }
-
-        let timer =
-          (minutos < 10 ? `0${minutos}` : minutos) +
-          ":" +
-          (segundos < 10 ? `0${segundos}` : segundos)
-
-        pomodoroTimer.innerText = timer
-        document.title = `${timer} - Tempo restante`
-      }
-    } 
-
+      timer(25);
+    }
+  
     if (shortBreakTimerTitle.classList.contains("ativo")) {
-      breakTime()
-      function breakTime() {
-        if (minutos == 0 && segundos == 0 && contador == 1) {
-          minutos = 5
-          segundos = 0
-          contador = 2
-        } else if (segundos <= 0 && minutos <= 0 && contador == 2) {
-          contador = 1
-          clearInterval(pausa)
-          pomodoroPause.style.display = "none"
-          pomodoroStart.style.display = "block"
-          shortBreakTimerTitle.classList.remove("ativo")
-          pomodoroTimerTitle.classList.add("ativo")
-          add()
-        } else if (pomodoroStart.style.display == "block") {
-          clearInterval(pausa)
-        } else if (
-          segundos > 0 &&
-          minutos > -1 &&
-          pomodoroStart.style.display == "none"
-        ) {
-          --segundos
-        } else if (segundos == 0) {
-          --minutos
-          segundos = 59
-        }
-
-        let timer =
-          (minutos < 10 ? `0${minutos}` : minutos) +
-          ":" +
-          (segundos < 10 ? `0${segundos}` : segundos)
-
-        pomodoroTimer.innerText = timer
-        document.title = `${timer} - Tempo restante`
-      }
-    } 
-  }, 1000)
+      timer(5);
+    }
 }
 
 function play() {
-  if (pomodoroStart.style.display == "block") {
-    pomodoroStart.style.display = "none"
-    pomodoroPause.style.display = "block"
-  } else {
-    pomodoroPause.style.display = "none"
-    pomodoroStart.style.display = "block"
-  }
-
-  if (pomodoroPause.style.display == "block") {
-    startPomodoro()
-  }
+  if (pomodoroStart.style.display !== "block") {
+    pomodoroPause.style.display = "none";
+    pomodoroStart.style.display = "block";
+    clearInterval(pausa)
+  } else if (pomodoroStart.style.display == "block") {
+    pomodoroStart.style.display = "none";
+    pomodoroPause.style.display = "block";
+    startPomodoro();
+  } 
 }
 
-function add() {
-  contador = 25
-  minutos = 0
-  segundos = 5
+function timer(tempo) {
+  pausa = setInterval(() => {
+    if (pomodoroStart.style.display == "block") {
+      clearInterval(pausa);
+    }
+    if (minutos == 0 && segundos == 0 && contador == 1) {
+      minutos = tempo;
+      segundos = 0;
+      contador = 2;
+    } else if (segundos <= 0 && minutos <= 0 && contador == 2) {
+      contador = 1;
+      clearInterval(pausa);
+      pomodoroPause.style.display = "none";
+      pomodoroStart.style.display = "block";
+      pomodoroTimerTitle.classList.remove("ativo");
+      shortBreakTimerTitle.classList.add("ativo");
+    } else if (
+      segundos > 0 &&
+      minutos > -1 &&
+      pomodoroStart.style.display == "none"
+    ) {
+      --segundos;
+    } else if (segundos == 0) {
+      --minutos;
+      segundos = 59;
+    }
+
+    let timer =
+      (minutos < 10 ? `0${minutos}` : minutos) +
+      ":" +
+      (segundos < 10 ? `0${segundos}` : segundos);
+
+    pomodoroTimer.innerText = timer;
+    document.title = `${timer} - Tempo restante`;
+  }, 1000)
 }
